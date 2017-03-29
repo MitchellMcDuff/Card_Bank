b5b7b707167cf28b7436df646954807287f12e1d
julianv6 <22julianv@gmail.com>
2017-03-30 00:02:19 +0100

    Add Players and updated UI
    add players button now works

--- MainApp.py
+++ MainApp.py
@@ -22,6 +22,9 @@ class MainMenu(QtGui.QMainWindow, Ui_MainWindow):
         self.goBackButton.clicked.connect(self.goBackToMainMenuButtonClick)
         self.goBack2Button.clicked.connect(self.goBackToMainMenuButtonClick)
         self.editCardsButton.clicked.connect(self.editCardsMenuButtonClick)
+        self.addPlayerButton.clicked.connect(self.addPlayer)
+        
+        #self.removePlayerButton.clicked.connect(self.removePlayer)
 
 
     def editCardsMenuButtonClick(self):
@@ -33,8 +36,26 @@ class MainMenu(QtGui.QMainWindow, Ui_MainWindow):
     def goBackToMainMenuButtonClick(self):
         self.stackedWidget.setCurrentIndex(0)
         
-
-        
+    def addPlayer(self):
+        #if there is an letter add the player to the list
+        if any(letter.isalpha() for letter in str(self.playerNameLineEdit.text())):
+            listItem = QtGui.QListWidgetItem(self.playerNameLineEdit.text())
+            tableItem = QtGui.QTableWidgetItem(self.playerNameLineEdit.text())
+            self.playerNameLineEdit.clear()
+            #check if player already exists
+            
+            #insert in list
+            self.playerListWidget.addItem(listItem)
+            #insert in score board
+            self.scoreBoardTableWidget.insertRow(self.scoreBoardTableWidget.rowCount())
+            self.scoreBoardTableWidget.setItem(self.scoreBoardTableWidget.rowCount()-1, 0, tableItem)
+            self.scoreBoardTableWidget.setItem(self.scoreBoardTableWidget.rowCount()-1, 1, QtGui.QTableWidgetItem("0"))
+            
+    
+    #def removePlayer(self):
+        #for item in self.playerListWidget.selectedItems():
+            #self.playerListWidget.takeItem(self.playerListWidget.row(item))
+        #for item in self.scoreBoardTableWidget.
         
         
 def main():
